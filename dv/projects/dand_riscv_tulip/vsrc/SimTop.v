630d5b1be86a694a4707ae1ffae20be39a28c57e
tracer-oscpu2021 <tracer@oscpu.org>
2026-08-02 11:14:05 +0800

    linyouxu 20210238    Static hostname: lin-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 621a0ba3d7054a65ab9e8c4bc9c8abe0            Boot ID: 5181a8582fe84400a965781226154bfb     Virtualization: vmware   Operating System: Ubuntu 20.04.6 LTS             Kernel: Linux 5.15.0-139-generic       Architecture: x86-64  11:14:05 up  9:48,  1 user,  load average: 1.09, 0.85, 0.50

--- dv/projects/dand_riscv_tulip/vsrc/SimTop.v
+++ dv/projects/dand_riscv_tulip/vsrc/SimTop.v
@@ -120,18 +120,17 @@ integer j;
 
 initial begin
   fd = $fopen ("/home/lin/DandProject/dv/bin/mytests/benchmarks/coremark/coremark-riscv64-nemu.bin", "rb");
-  // fd = $fopen ("/home/lin/DandProject/dv/bin/mytests/fc_tests/mario/fceux-riscv64-nemu.bin", "rb");
   
   tmp = $fread(ram_tmp, fd);
   for (i = 0; i < 65536; i = i + 1) begin
-    // SimTop.u_Tulip.icache.sram_6.mem_symbol0[i] = ram_tmp[i*4 + 0][7:0];
-    // SimTop.u_Tulip.icache.sram_6.mem_symbol1[i] = ram_tmp[i*4 + 1][7:0];
-    // SimTop.u_Tulip.icache.sram_6.mem_symbol2[i] = ram_tmp[i*4 + 2][7:0];
-    // SimTop.u_Tulip.icache.sram_6.mem_symbol3[i] = ram_tmp[i*4 + 3][7:0];
-    SimTop.u_Tulip.icache.sram_8.mem_symbol0[i] = ram_tmp[i*4 + 0][7:0];
-    SimTop.u_Tulip.icache.sram_8.mem_symbol1[i] = ram_tmp[i*4 + 1][7:0];
-    SimTop.u_Tulip.icache.sram_8.mem_symbol2[i] = ram_tmp[i*4 + 2][7:0];
-    SimTop.u_Tulip.icache.sram_8.mem_symbol3[i] = ram_tmp[i*4 + 3][7:0];
+    SimTop.u_Tulip.icache.sram_6.mem_symbol0[i] = ram_tmp[i*4 + 0][7:0];
+    SimTop.u_Tulip.icache.sram_6.mem_symbol1[i] = ram_tmp[i*4 + 1][7:0];
+    SimTop.u_Tulip.icache.sram_6.mem_symbol2[i] = ram_tmp[i*4 + 2][7:0];
+    SimTop.u_Tulip.icache.sram_6.mem_symbol3[i] = ram_tmp[i*4 + 3][7:0];
+    // SimTop.u_Tulip.icache.sram_8.mem_symbol0[i] = ram_tmp[i*4 + 0][7:0];
+    // SimTop.u_Tulip.icache.sram_8.mem_symbol1[i] = ram_tmp[i*4 + 1][7:0];
+    // SimTop.u_Tulip.icache.sram_8.mem_symbol2[i] = ram_tmp[i*4 + 2][7:0];
+    // SimTop.u_Tulip.icache.sram_8.mem_symbol3[i] = ram_tmp[i*4 + 3][7:0];
   end
 end
 
